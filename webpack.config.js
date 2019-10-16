3b2139cdbd44b5a799eb3b3ef49c987dc918a739
ksPr <h2x@pm.me>
2019-10-16 15:07:37 +0900

    feat : add vuex-module-decorators

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 // webpack.config.js
 
 const VueAutoRoutingPlugin = require('vue-auto-routing/lib/webpack-plugin');
+const path = require('path');
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
 
 module.exports = {
   // ... other options ...
-
   plugins: [
     new VueAutoRoutingPlugin({
       // Path to the directory that contains your page components.
@@ -13,21 +14,11 @@ module.exports = {
       // A string that will be added to importing component path (default @/pages/).
       importPrefix: '@/pages/',
     }),
+    new PrerenderSPAPlugin({
+      // Required - The path to the webpack-outputted app to prerender.
+      staticDir: path.join(__dirname, 'dist'),
+      // Required - Routes to render.
+      routes: ['/', '/about'],
+    }),
   ],
-  pluginOptions: {
-    prerenderSpa: {
-      registry: undefined,
-      renderRoutes: ['/', '/about'],
-      useRenderEvent: true,
-      headless: true,
-      onlyProduction: true,
-      postProcess: (route) => {
-        // Defer scripts and tell Vue it's been server rendered to trigger hydration
-        route.html = route.html
-          .replace(/<script (.*?)>/g, '<script $1 defer>')
-          .replace('id="app"', 'id="app" data-server-rendered="true"');
-        return route;
-      },
-    },
-  },
 };
